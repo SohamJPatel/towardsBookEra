ec2f7acaefadfae8d6abf5387623368915bccbe0
SohamJPatel <sohamjpatel8979@gmail.com>
2020-10-19 15:35:21 +0530

    Created basic cart front end

--- bookApp/urls.py
+++ bookApp/urls.py
@@ -4,4 +4,5 @@ from . import views as book_view
 
 urlpatterns = [
     path('', book_view.get_all_books, name="bookshopPath"),
+    path('bookview/<int:book_id>', book_view.bookview, name="bookViewPath"),
 ]
--- bookApp/views.py
+++ bookApp/views.py
@@ -23,7 +23,13 @@ def get_all_books(request):
         context = {'books' : book_list}
         return render(request, "Bookspage.html", context)
 
-    books = Book.objects.all();
+    books = Book.objects.all()
     book_list = list(books)
     context = {'books': book_list}
     return render(request, "Bookspage.html", context)
+
+def bookview(request, book_id):
+    book = Book.objects.filter(id=book_id)
+    context = {'bookdata' : book[0]}
+    print(book)
+    return render(request, "Book.html", context)
